a28e568b953ff26ee3df58da28447d837413f228
JarinManuel <40002275+JarinManuel@users.noreply.github.com>
2018-07-19 16:15:23 -0700

    Update HashingTest.rb

--- HashingTest.rb
+++ HashingTest.rb
@@ -1,11 +1,17 @@
+#require 'csv'
+
+#OpenHash = {}
+
+#CSV.foreach("TestingHash.csv") do |line| 
+
+#  OpenHash[line[0].to_i] = [line[1].strip, line[2].to_f]
+
+#end
+
+#puts OpenHash
+
 require 'csv'
-
-OpenHash = {}
-
-CSV.foreach("TestingHash.csv") do |line| 
-
-  OpenHash[line[0].to_i] = [line[1].strip, line[2].to_f]
-
-end
-
-puts OpenHash
+csv_data = CSV.read 'data.csv'
+headers = csv_data.shift.map {|i| i.to_s }
+string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
+array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
