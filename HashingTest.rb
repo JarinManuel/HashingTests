ceb2efd7ed638fe6f7950ec5cca0b929d1adfc6d
JarinManuel <40002275+JarinManuel@users.noreply.github.com>
2018-07-20 10:57:24 -0700

    Update HashingTest.rb

--- HashingTest.rb
+++ HashingTest.rb
@@ -25,3 +25,5 @@ tickers = {}
 CSV.foreach("TestingHash.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
   tickers[row.fields[0]] = Hash[row.headers[1..-1].zip(row.fields[1..-1])]
 end
+
+puts Food[1]
