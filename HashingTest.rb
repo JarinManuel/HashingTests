0257c71a6e18e2df65adf261b0c8ccd30ba38c76
JarinManuel <40002275+JarinManuel@users.noreply.github.com>
2018-07-20 10:52:44 -0700

    Update HashingTest.rb

--- HashingTest.rb
+++ HashingTest.rb
@@ -10,10 +10,18 @@
 
 #puts OpenHash
 
-require 'csv'
-csv_data = CSV.read 'TestingHash.csv'
-headers = csv_data.shift.map {|i| i.to_s }
-string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
-array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
+#require 'csv'
+#csv_data = CSV.read 'TestingHash.csv'
+#headers = csv_data.shift.map {|i| i.to_s }
+#string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
+#array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
 
-puts string_data[2]
+#puts string_data[2]
+
+require 'csv'
+
+tickers = {}
+
+CSV.foreach("TestingHash.csv", :headers => true, :header_converters => :symbol, :converters => :all) do |row|
+  tickers[row.fields[0]] = Hash[row.headers[1..-1].zip(row.fields[1..-1])]
+end
