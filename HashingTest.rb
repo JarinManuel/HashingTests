1dfd35007779e194483bac853d3020fa1a662940
Jarin Manuel <jarin.manuel@spirent.com>
2018-07-23 10:34:19 -0700

    Getting specific

--- HashingTest.rb
+++ HashingTest.rb
@@ -35,4 +35,6 @@ headers = csv_data.shift.map {|i| i.to_s }
 string_data = csv_data.map {|row| row.map {|cell| cell.to_s } }
 array_of_hashes = string_data.map {|row| Hash[*headers.zip(row).flatten] }
 
-puts headers
+puts headers[1]
+
+puts string_data
